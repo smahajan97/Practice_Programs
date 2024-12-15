4aa89cee653f8b56ca636a0989ca7f65885d831b
smahajan97 <136894004+smahajan97@users.noreply.github.com>
2024-12-15 12:50:53 +0530

    twoSum.java
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].

---
+++ twoSum.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        for(int i = 0 ; i <= nums.length ; i++) {
+            for(int j = i+1 ; j < nums.length ; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return null;
+    }
+}
+public class First {
+    public static void main(String[] args) {
+
+        Solution solution = new Solution();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int result[] = solution.twoSum(nums, target);
+
+        if (result != null) {
+            System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
